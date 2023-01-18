5b58f4c257c27d8a8334d56d8df4b326f0d8f107
Emanuel Seidinger <dev@eseidinger.de>
2023-01-18 07:03:29 +0100

    minor fix in node install

--- tools/install_node.sh
+++ tools/install_node.sh
@@ -16,7 +16,7 @@ source ${SCRIPT_DIR}/versions.sh
 
 wget -q -P ~/ https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}-linux-${ARCH}.tar.xz
 mkdir -p /usr/local/lib/nodejs
-tar -xJvf node-v${NODE_VERSION}-linux-${ARCH}.tar.xz -C /usr/local/lib/nodejs
+tar -xJvf ~/node-v${NODE_VERSION}-linux-${ARCH}.tar.xz -C /usr/local/lib/nodejs
 rm ~/node-v${NODE_VERSION}-linux-${ARCH}.tar.xz
 
 mv /usr/local/lib/nodejs/node-v${NODE_VERSION}-linux-${ARCH} /usr/local/lib/nodejs/node
