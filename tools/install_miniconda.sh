e48224d972288b6cc44b2c8334a3d8299ba85567
Emanuel Seidinger <dev@eseidinger.de>
2023-02-13 18:05:47 +0100

    fix docker build

--- tools/install_miniconda.sh
+++ tools/install_miniconda.sh
@@ -4,7 +4,7 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SCRIPT_DIR}/versions.sh
 
 wget -q -P ~/ https://repo.anaconda.com/miniconda/Miniconda3-${MINICONDA_VERSION}-Linux-$(uname -m).sh
-bash ~/Miniconda3-${MINICONDA_VERSION}-Linux-$(uname -m).sh -b -p
+bash ~/Miniconda3-${MINICONDA_VERSION}-Linux-$(uname -m).sh -b -p ${HOME}/miniconda3
 rm ~/Miniconda3-${MINICONDA_VERSION}-Linux-$(uname -m).sh
 source ~/miniconda3/bin/activate
 conda init
