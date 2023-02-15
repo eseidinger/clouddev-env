b4b449777427a4c239ae555ea8e0e03e721a1197
Emanuel Seidinger <dev@eseidinger.de>
2023-02-15 09:50:55 +0100

    add chromium install scripts

--- tools/install.sh
+++ tools/install.sh
@@ -30,3 +30,5 @@ bash ${SCRIPT_DIR}/install_node.sh
 bash ${SCRIPT_DIR}/install_k9s.sh
 bash ${SCRIPT_DIR}/config_k9s.sh
 bash ${SCRIPT_DIR}/install_java.sh
+bash ${SCRIPT_DIR}/install_chromium.sh
+bash ${SCRIPT_DIR}/config_chromium.sh
