7671dc5ea39a9ea5fdde6eeffe7104a3e338c370
Emanuel Seidinger <dev@eseidinger.de>
2023-02-15 13:18:49 +0100

    fix docker node

--- tools/config_node_docker.sh
+++ tools/config_node_docker.sh
@@ -3,4 +3,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SCRIPT_DIR}/versions.sh
 
-move ~/.nvm/versions/node/v${NODE_VERSION} ~/.nvm/versions/node/current
+mv ~/.nvm/versions/node/v${NODE_VERSION} ~/.nvm/versions/node/current
