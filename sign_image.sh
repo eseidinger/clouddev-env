eea8b73ed34a150bba946dcc48ea7343e5298fd0
Emanuel Seidinger <dev@eseidinger.de>
2023-04-14 07:50:09 +0200

    add image verification script

--- sign_image.sh
+++ sign_image.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:efc2fd8d43f126d66cdbaca9ffbca786b045572348a0adc844e3e6685a44d1cd
+cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:036d165f163e4969b870ba2662053904fb75ee89788802ea6257da3f4707958c
---
+++ verify_image.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+cosign verify --key cosign.pub harbor.eseidinger.de/public/cloud-tools@sha256:036d165f163e4969b870ba2662053904fb75ee89788802ea6257da3f4707958c
