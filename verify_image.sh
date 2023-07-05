bf2ea28996d783aee68be197481f0d124197e911
Emanuel Seidinger <dev@eseidinger.de>
2023-07-05 11:16:45 +0200

    update image sha

--- verify_image.sh
+++ verify_image.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-cosign verify --key cosign.pub harbor.eseidinger.de/public/cloud-tools@sha256:3af4d568b00795c147d05a04e120d8adf237c821eaf8d146eb08c649c95cc395
+cosign verify --key cosign.pub harbor.eseidinger.de/public/cloud-tools@sha256:db39db03113c8480effa91fb92c5e3ef6d2093033e126e4bccfcd81cfa31d871
