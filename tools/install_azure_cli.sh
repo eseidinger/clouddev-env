65e83cf5f6626683eddd61e527d79b61b8edfbf4
Emanuel Seidinger <dev@eseidinger.de>
2024-06-04 20:08:55 +0200

    fix azure cli install script

--- tools/install_azure_cli.sh
+++ tools/install_azure_cli.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source ${SCRIPT_DIR}/versions.sh
 
+source ~/miniconda3/bin/activate base
 pip install --force-reinstall -v "azure-cli==${AZURE_CLI_VERSION}"
