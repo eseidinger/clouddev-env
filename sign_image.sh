16f7c1d44b08cdc04bdb08088359bbb060f4312e
Emanuel Seidinger <dev@eseidinger.de>
2024-06-09 18:13:16 +0200

    update image hash

--- sign_image.sh
+++ sign_image.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:c4fa3c447cf489522a91908ea3a1c36e556a518b5f1d36a8e3f5b48338224a45
-cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:00dcc32cdfd41c4f833da37dadaa25882b3beba15a1319bfd4f5d969d49738e2
-cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:01b865b954a201584f46fa01542a7dace163388000ebf8628fb4b45ca98a3e79
+cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:2617d1b76825ef75f6549f0d4e950edfdf78718405ae2ac469ee5e0d308896ec
+cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:e62f082f4f8c98112aa6d7022481c75138b77c4d3cb912a57a97c3b62bd97f0b
+cosign sign --key secret/cosign.key harbor.eseidinger.de/public/cloud-tools@sha256:586c59360ea7c03729ca2f4e6b01d65ffe0c226c6c064aa28e60685c77386a55
